d8f0f06f7895ace43f6b08d575b143fac289a1be
Tvgo77 <tvgoshao77@gmail.com>
2024-04-04 06:04:03 -0500

    Go and PostgreSQL docker environment setting complete

---
+++ golang/go.mod
@@ -0,0 +1,10 @@
+module hello
+
+go 1.22.0
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.32.0
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ golang/src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("This is a test message.")
+}
